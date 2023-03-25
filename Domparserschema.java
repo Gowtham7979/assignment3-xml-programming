233dc320cca88ebbb4ba9df3be97eba9bc7384e1
Gowtham7979 <126810886+Gowtham7979@users.noreply.github.com>
2023-03-25 13:49:16 -0400

    Update Domparserschema.java

--- Domparserschema.java
+++ Domparserschema.java
@@ -13,18 +13,12 @@ import org.w3c.dom.Document;
 public class Domparserschema {
   public static void main(String[] args) {
     try {
-      // Create a DocumentBuilderFactory and DocumentBuilder
+     
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       DocumentBuilder builder = factory.newDocumentBuilder();
-
-      // Parse the XML document into a Document
-      Document doc = builder.parse("cinema.xml");
-
-      // Create a SchemaFactory and Schema from the XSD file
+ Document doc = builder.parse("cinema.xml");
       SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
       Schema schema = schemaFactory.newSchema(new File("cinema.xsd"));
-
-      // Create a Validator and validate the Document against the Schema
       Validator validator = schema.newValidator();
       validator.validate(new DOMSource(doc));
       
